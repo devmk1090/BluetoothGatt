8543ea7eda707d43f9295ea78938788d404a8f90
devmk1090 <devmk1090@gmail.com>
2024-04-14 04:21:31 +0900

    Notification - 버전에 따른 채널 추가 - 노티 클릭시 화면 이동을 위한 PendingIntent 추가

--- app/src/main/java/com/devk/bluetoothgatt/BleGattService.kt
+++ app/src/main/java/com/devk/bluetoothgatt/BleGattService.kt
@@ -1,10 +1,14 @@
 package com.devk.bluetoothgatt
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
 import com.devk.bluetoothgatt.response.DeviceResponse
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -34,6 +38,8 @@ class BleGattService: Service() {
     private var devices: List<DeviceResponse>? = null
     private var devicesName: ArrayList<String>? = null
 
+    private var notification: NotificationCompat.Builder? = null
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         when (intent?.action) {
             BLE_GATT_START -> {
@@ -44,10 +50,51 @@ class BleGattService: Service() {
                 }
                 if (!devices.isNullOrEmpty()) {
                     devicesName = devices?.map { it.name } as ArrayList<String>
+                    setNotification()
                 }
             }
         }
         return START_STICKY
+    }
+
+    private fun setNotification() {
+        if (notification == null) {
+            val channelId = setNotificationChannel()
+            notification = NotificationCompat.Builder(context, channelId)
+            notification?.let {
+                it.priority = NotificationCompat.PRIORITY_DEFAULT
+                it.setOngoing(true)
+                it.setSmallIcon(R.mipmap.ic_launcher)
+                it.setContentTitle("Title")
+                it.setContentText("Content...")
+
+                val intent = Intent(context, MainActivity::class.java)
+                intent.action = Intent.ACTION_VIEW
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    val pendingIntent =
+                        PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+                    it.setContentIntent(pendingIntent)
+                } else {
+                    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+                    it.setContentIntent(pendingIntent)
+                }
+                startForeground(1, it.build())
+            }
+        }
+    }
+
+    private fun setNotificationChannel(): String {
+        val id = "BluetoothGatt"
+        val name = "BluetoothGatt Background Service"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance)
+            channel.setShowBadge(false)
+            channel.enableVibration(false)
+            val notificationService = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationService.createNotificationChannel(channel)
+        }
+        return id
     }
 
     override fun onBind(p0: Intent?): IBinder? {
